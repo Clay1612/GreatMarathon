778e902dd017dae3d78bd33082b08d094fcd85dd
Nick <dedsec1612@gmail.com>
2021-12-15 15:40:17 +0300

    ToDoList v2.0 stable

--- Day49_ToDoListUI/main.js
+++ Day49_ToDoListUI/main.js
@@ -1,14 +1,30 @@
-import {inputs} from "./view.js";
+import {createNewTask, inputs} from "./view.js";
 
+for (let task of inputs.addNewTask) {
+    task.addEventListener('submit', function () {
+        let currentTask = undefined;
+        if ( task.classList.contains('high-priority') ) {
+            currentTask = createNewTask(inputs.newTaskValue[0].value);
+            task.after(currentTask);
+            inputs.newTaskValue[0].value = '';
+        } else if ( task.classList.contains('low-priority') ) {
+            currentTask = createNewTask(inputs.newTaskValue[1].value)
+            task.after(currentTask);
+            inputs.newTaskValue[1].value = '';
+        }
 
-/*
-Необходимо реализовать следующий функционал:
-1) При нажатии на + или enter создается новая задача, которая добавляется в ту или иную колонку,
-в зависимости от приоритета. (классы high-priority и low-priority)
-2) Value из input для создания задачи переносится в value text input новой задачи (класс add-task)
-3) При нажатии на checkbox:
-    3.1) У псевдоэлемента before меняется background на серый (.todolist__checkbox-field+label::before)
-    3.2) У text input меняется background на серый (todolist__task)
-    3.3) Таска становится неактивной
-4) При нажатии на крестик в таске, данная таска удаляется.
-*/
+        const deleteTaskButton = currentTask.querySelector('.todolist__delete-button');
+
+        deleteTaskButton.addEventListener('click', function () {
+            currentTask.remove();
+        })
+
+        const doneCheckBoxLabel = currentTask.querySelector('.todolist__checkbox-label');
+        const taskValue = currentTask.querySelector('.todolist__text-field');
+
+        doneCheckBoxLabel.addEventListener('click', function () {
+            doneCheckBoxLabel.classList.toggle('active');
+            taskValue.classList.toggle('active');
+        })
+    })
+}
--- Day49_ToDoListUI/view.js
+++ Day49_ToDoListUI/view.js
@@ -1,7 +1,32 @@
 export const inputs = {
-    newTaskValue: document.querySelectorAll('.add-task'),
-    addNewTask: document.querySelectorAll('.todolist__add-task-button'),
-    doneCheckBox: document.querySelectorAll('.todolist__checkbox-field'),
-    taskValue: document.querySelectorAll('.todolist__task'),
-    deleteTask: document.querySelectorAll('.todolist__delete-button'),
+    addNewTask: document.querySelectorAll('.todolist__add-task-field'),
+    newTaskValue: document.querySelectorAll('.todolist__newText-field'),
+}
+
+export function createNewTask(taskValue) {
+    let newTask = document.createElement('form');
+    newTask.className = 'todolist__task';
+
+    let doneCheckBox = document.createElement('input');
+    doneCheckBox.type = 'checkbox';
+    doneCheckBox.className = 'todolist__checkbox-field';
+    doneCheckBox.id = 'done-checkbox';
+
+    let doneCheckBoxLabel = document.createElement('label');
+    doneCheckBoxLabel.for = 'done-checkbox';
+    doneCheckBoxLabel.className = 'todolist__checkbox-label';
+
+    let taskText = document.createElement('input');
+    taskText.type = 'text';
+    taskText.className = 'todolist__text-field';
+    taskText.value = taskValue;
+    taskText.disabled = true;
+
+    let deleteButton = document.createElement('input');
+    deleteButton.type = 'button';
+    deleteButton.className = 'todolist__delete-button';
+
+    newTask.append( doneCheckBox, doneCheckBoxLabel, taskText, deleteButton );
+
+    return newTask;
 }
