7a4037a72a2006ebf586478fc84a3015b7c84cd3
Nick <dedsec1612@gmail.com>
2021-12-15 08:53:33 +0300

    ÑDay49 done

---
+++ Day49_ToDoListUI/view.js
@@ -0,0 +1,7 @@
+export const inputs = {
+    newTaskValue: document.querySelectorAll('.add-task'),
+    addNewTask: document.querySelectorAll('.todolist__add-task-button'),
+    doneCheckBox: document.querySelectorAll('.todolist__checkbox-field'),
+    taskValue: document.querySelectorAll('.todolist__task'),
+    deleteTask: document.querySelectorAll('.todolist__delete-button'),
+}
