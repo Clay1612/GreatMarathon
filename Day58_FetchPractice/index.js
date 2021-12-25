be59fad918bce348d54c078a0d54ef174e2783c5
Nick <dedsec1612@gmail.com>
2021-12-25 11:31:21 +0300

    Check Sex interface v1.0

--- Day58_FetchPractice/index.js
+++ Day58_FetchPractice/index.js
@@ -1,2 +1,22 @@
 'use strict'
-console.log('Test');
+const nameField = document.querySelector('.form__name-input');
+const checkSexForm = document.querySelector('.content__form');
+
+checkSexForm.addEventListener('submit', function () {
+    event.preventDefault()         //Basic fix of page reload at the beginning of script execution
+
+    const firstName = nameField.value;
+    const serverUrl = 'https://api.genderize.io';
+    const url = `${serverUrl}?name=${firstName}`;
+    const result = document.querySelector('.content__result-field');
+
+    let promise = fetch(url);
+    promise.then( function (response) {
+        let json = response.json();
+        json.then( function (value) {
+            result.textContent = `${firstName} is ${value.gender}`;
+        })
+    })
+
+    nameField.value = '';
+})
