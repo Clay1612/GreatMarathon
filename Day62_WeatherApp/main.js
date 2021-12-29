a1393c5a7678ac03e86cfb5f36db97a7f31b19bd
Nick <dedsec1612@gmail.com>
2021-12-29 07:06:45 +0300

    Ñerror handling added

--- Day62_WeatherApp/main.js
+++ Day62_WeatherApp/main.js
@@ -3,10 +3,17 @@ import {UI_ELEMENTS} from './view.js';
 const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f&units=metric';
 
+window.addEventListener('unhandledrejection', function (error) {
+    alert(`Sorry, ${error.reason}, script failed`);
+})
+
 function serverRequest(url) {
     return fetch(url)
         .then( function (response){
-        return response.json()
+            if (response.status === 404) {
+                throw new Error('404');
+            }
+            return response.json()
     })
 }
 
@@ -20,6 +27,18 @@ function getWeatherInfo() {
             UI_ELEMENTS.nowDisplay.temperature.innerHTML = `${ Math.round(cityInfo.main.temp) }&#176`;
             UI_ELEMENTS.nowDisplay.city.textContent = cityInfo.name;
             UI_ELEMENTS.nowDisplay.condition.style.backgroundImage = `url("http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}@2x.png")`
+        })
+        .catch(function (error) {
+            switch (error.message) {
+                case 'Failed to fetch':
+                    alert(`Error: Server request failed`);
+                    break;
+                case '404':
+                    alert('Error: city is not found');
+                    break;
+                default:
+                    throw new Error('Unexpected error');
+            }
         })
 }
 
