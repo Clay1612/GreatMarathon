630c6f54875390c2158b2fab8fa399c139b50cc9
Nick <dedsec1612@gmail.com>
2022-01-25 07:13:47 +0300

    initialization npm test project

---
+++ Day102_Npm/main.js
@@ -0,0 +1,30 @@
+import {createNewTask, inputs} from "./view.js";
+
+for (let task of inputs.addNewTask) {
+    task.addEventListener('submit', function () {
+        let currentTask = undefined;
+        if ( task.classList.contains('high-priority') ) {
+            currentTask = createNewTask(inputs.newTaskValue[0].value);
+            task.after(currentTask);
+            inputs.newTaskValue[0].value = '';
+        } else if ( task.classList.contains('low-priority') ) {
+            currentTask = createNewTask(inputs.newTaskValue[1].value)
+            task.after(currentTask);
+            inputs.newTaskValue[1].value = '';
+        }
+
+        const deleteTaskButton = currentTask.querySelector('.todolist__delete-button');
+
+        deleteTaskButton.addEventListener('click', function () {
+            currentTask.remove();
+        })
+
+        const doneCheckBoxLabel = currentTask.querySelector('.todolist__checkbox-label');
+        const taskValue = currentTask.querySelector('.todolist__text-field');
+
+        doneCheckBoxLabel.addEventListener('click', function () {
+            doneCheckBoxLabel.classList.toggle('active');
+            taskValue.classList.toggle('active');
+        })
+    })
+}
