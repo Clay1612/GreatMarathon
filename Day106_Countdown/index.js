81d8188dffd882d00d0e387c33b28df57d66a3ca
Nick <dedsec1612@gmail.com>
2022-02-06 16:41:16 +0300

    countdown app done v2.0

--- Day106_Countdown/index.js
+++ Day106_Countdown/index.js
@@ -1,20 +1,21 @@
 import { intervalToDuration, formatDuration  } from 'date-fns';
 
-const countButton = document.querySelector('.content__countdown-button');
-const dateField = document.querySelector('.content__enter-date-field');
-const timerField = document.querySelector('.content__countdown-timer-field');
+const UI_ELEMENTS = {
+    countButton: document.querySelector('.content__countdown-button'),
+    dateField: document.querySelector('.content__enter-date-field'),
+    timerField: document.querySelector('.content__countdown-timer-field'),
+}
 
-countButton.addEventListener('click', function (){
-    if (Date.now() < new Date(dateField.value) - 10800000) {
+UI_ELEMENTS.countButton.addEventListener('click', function (){
+    if (Date.now() < new Date(UI_ELEMENTS.dateField.value) - 10800000) {
         const interval = intervalToDuration({
             start: Date.now(),
-            end: new Date(dateField.value) - 10800000,
-        })
-
-        timerField.value = formatDuration(interval);
+            end: new Date(UI_ELEMENTS.dateField.value) - 10800000,
+        });
+        UI_ELEMENTS.timerField.value = formatDuration(interval);
     } else {
-        timerField.value = 'Choose date after current date'
+        UI_ELEMENTS.timerField.value = 'Choose date after current date';
     }
-})
+});
 
 
