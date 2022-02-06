8be5acd8fd1849319c8f0daae048601d3d79d595
Nick <dedsec1612@gmail.com>
2022-02-06 14:10:30 +0300

    Countdown v.1.0 done

--- Day106_Countdown/index.js
+++ Day106_Countdown/index.js
@@ -1,2 +1,20 @@
-const date = document.querySelector('.content__enter-date-field').value;
-console.log(date);
+import { intervalToDuration, formatDuration  } from 'date-fns';
+
+const countButton = document.querySelector('.content__countdown-button');
+const dateField = document.querySelector('.content__enter-date-field');
+const timerField = document.querySelector('.content__countdown-timer-field');
+
+countButton.addEventListener('click', function (){
+    if (Date.now() < new Date(dateField.value) - 10800000) {
+        const interval = intervalToDuration({
+            start: Date.now(),
+            end: new Date(dateField.value) - 10800000,
+        })
+
+        timerField.value = formatDuration(interval);
+    } else {
+        timerField.value = 'Choose date after current date'
+    }
+})
+
+
