3eccc35bed66de2aa042c087822b291a44665083
Nick <dedsec1612@gmail.com>
2022-02-26 18:20:08 +0300

    Fix overflow and scroll

--- Day124_chat/main.js
+++ Day124_chat/main.js
@@ -1,1 +1,21 @@
-console.log('test');
+const UI_ELEMENTS = {
+    messageTemplate: document.querySelector('.chat-content__message-template'),
+    sentMessageForm: document.querySelector('.chat-footer__form'),
+    chatContent: document.querySelector('.chat-content'),
+};
+
+
+UI_ELEMENTS.sentMessageForm.addEventListener('submit', function () {
+    addMessage()
+});
+
+function addMessage() {
+    event.preventDefault();
+
+    const message = UI_ELEMENTS.messageTemplate.content.cloneNode(true);
+
+    message.querySelector('.chat-content__text').textContent = document.querySelector('.chat-footer__message-input').value;
+    UI_ELEMENTS.chatContent.append(message);
+    UI_ELEMENTS.chatContent.scrollTop = UI_ELEMENTS.chatContent.scrollHeight;
+    document.querySelector('.chat-footer__message-input').value = '';
+}
