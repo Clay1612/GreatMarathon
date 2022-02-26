4a5163e2fdd9a75348a4eef2b035f0dd48b8955b
Nick <dedsec1612@gmail.com>
2022-02-26 19:44:46 +0300

    basic functionaly added

--- Day124_chat/main.js
+++ Day124_chat/main.js
@@ -1,21 +1,42 @@
-const UI_ELEMENTS = {
-    messageTemplate: document.querySelector('.chat-content__message-template'),
-    sentMessageForm: document.querySelector('.chat-footer__form'),
-    chatContent: document.querySelector('.chat-content'),
-};
+import {UI_ELEMENTS} from './view.js'
 
+UI_ELEMENTS.sentMessageForm.addEventListener('submit', addMessage);
 
-UI_ELEMENTS.sentMessageForm.addEventListener('submit', function () {
-    addMessage()
-});
+UI_ELEMENTS.optionsButton.addEventListener('click', function () {
+    UI_ELEMENTS.popups.popupOverlay.style.display = 'block';
+    UI_ELEMENTS.popups.optionsPopup.style.display = 'grid';
+})
+
+UI_ELEMENTS.popups.optionsCloseButton.addEventListener('click', function () {
+    UI_ELEMENTS.popups.popupOverlay.style.display = 'none';
+    UI_ELEMENTS.popups.optionsPopup.style.display = 'none';
+})
 
 function addMessage() {
     event.preventDefault();
 
     const message = UI_ELEMENTS.messageTemplate.content.cloneNode(true);
 
-    message.querySelector('.chat-content__text').textContent = document.querySelector('.chat-footer__message-input').value;
-    UI_ELEMENTS.chatContent.append(message);
-    UI_ELEMENTS.chatContent.scrollTop = UI_ELEMENTS.chatContent.scrollHeight;
-    document.querySelector('.chat-footer__message-input').value = '';
+    if (document.querySelector('.chat-footer__message-input').value) {
+        message.querySelector('.chat-content__text').textContent = document.querySelector('.chat-footer__message-input').value;
+        message.querySelector('.chat-content__time').textContent = getCurrentTime();
+        UI_ELEMENTS.chatContent.append(message);
+
+        UI_ELEMENTS.chatContent.scrollTop = UI_ELEMENTS.chatContent.scrollHeight;
+        document.querySelector('.chat-footer__message-input').value = '';
+    }
+}
+
+function getCurrentTime() {
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    let time;
+
+    if (minutes <= 9) {
+        time = `${hours}:0${minutes}`;
+    } else {
+        time = `${hours}:${minutes}`;
+    }
+
+    return time;
 }
---
+++ Day124_chat/view.js
@@ -0,0 +1,11 @@
+export const UI_ELEMENTS = {
+    messageTemplate: document.querySelector('.chat-content__message-template'),
+    sentMessageForm: document.querySelector('.chat-footer__form'),
+    chatContent: document.querySelector('.chat-content'),
+    optionsButton: document.querySelector('.chat-header__options-button'),
+    popups: {
+        popupOverlay: document.querySelector('.popup-wrapper'),
+        optionsPopup: document.querySelector('.options-popup'),
+        optionsCloseButton: document.querySelector('.options-popup__close-button'),
+    },
+};
