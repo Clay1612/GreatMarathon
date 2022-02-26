4182863814e08c11b87c8aa8f28ef5d0a466e544
Nick <dedsec1612@gmail.com>
2022-02-26 21:22:58 +0300

    popups logic ready v1.0

--- Day124_chat/main.js
+++ Day124_chat/main.js
@@ -1,42 +1,64 @@
-import {UI_ELEMENTS} from './view.js'
+import {UI_ELEMENTS} from './view.js';
 
 UI_ELEMENTS.sentMessageForm.addEventListener('submit', addMessage);
 
 UI_ELEMENTS.optionsButton.addEventListener('click', function () {
-    UI_ELEMENTS.popups.popupOverlay.style.display = 'block';
-    UI_ELEMENTS.popups.optionsPopup.style.display = 'grid';
-})
+	UI_ELEMENTS.popups.popupOverlay.style.display = 'block';
+	UI_ELEMENTS.popups.optionsPopup.style.display = 'grid';
+});
 
 UI_ELEMENTS.popups.optionsCloseButton.addEventListener('click', function () {
-    UI_ELEMENTS.popups.popupOverlay.style.display = 'none';
-    UI_ELEMENTS.popups.optionsPopup.style.display = 'none';
-})
+	UI_ELEMENTS.popups.popupOverlay.style.display = 'none';
+	UI_ELEMENTS.popups.optionsPopup.style.display = 'none';
+});
+
+UI_ELEMENTS.authorizationButton.addEventListener('click', function () {
+	UI_ELEMENTS.popups.popupOverlay.style.display = 'block';
+	UI_ELEMENTS.popups.authorizationPopup.style.display = 'grid';
+});
+
+UI_ELEMENTS.popups.authorizationCloseButton.addEventListener('click', function () {
+	UI_ELEMENTS.popups.popupOverlay.style.display = 'none';
+	UI_ELEMENTS.popups.authorizationPopup.style.display = 'none';
+});
+
+UI_ELEMENTS.popups.authorizationForm.addEventListener('submit', function () {
+	event.preventDefault();
+
+	UI_ELEMENTS.popups.authorizationPopup.style.display = 'none';
+	UI_ELEMENTS.popups.confirmationPopup.style.display = 'grid';
+});
+
+UI_ELEMENTS.popups.confirmationCloseButton.addEventListener('click', function () {
+	UI_ELEMENTS.popups.popupOverlay.style.display = 'none';
+	UI_ELEMENTS.popups.confirmationPopup.style.display = 'none';
+});
 
 function addMessage() {
-    event.preventDefault();
+	event.preventDefault();
 
-    const message = UI_ELEMENTS.messageTemplate.content.cloneNode(true);
+	const message = UI_ELEMENTS.messageTemplate.content.cloneNode(true);
 
-    if (document.querySelector('.chat-footer__message-input').value) {
-        message.querySelector('.chat-content__text').textContent = document.querySelector('.chat-footer__message-input').value;
-        message.querySelector('.chat-content__time').textContent = getCurrentTime();
-        UI_ELEMENTS.chatContent.append(message);
+	if (document.querySelector('.chat-footer__message-input').value) {
+		message.querySelector('.chat-content__text').textContent = document.querySelector('.chat-footer__message-input').value;
+		message.querySelector('.chat-content__time').textContent = getCurrentTime();
+		UI_ELEMENTS.chatContent.append(message);
 
-        UI_ELEMENTS.chatContent.scrollTop = UI_ELEMENTS.chatContent.scrollHeight;
-        document.querySelector('.chat-footer__message-input').value = '';
-    }
+		UI_ELEMENTS.chatContent.scrollTop = UI_ELEMENTS.chatContent.scrollHeight;
+		document.querySelector('.chat-footer__message-input').value = '';
+	}
 }
 
 function getCurrentTime() {
-    const hours = new Date().getHours();
-    const minutes = new Date().getMinutes();
-    let time;
+	const hours = new Date().getHours();
+	const minutes = new Date().getMinutes();
+	let time;
 
-    if (minutes <= 9) {
-        time = `${hours}:0${minutes}`;
-    } else {
-        time = `${hours}:${minutes}`;
-    }
+	if (minutes <= 9) {
+		time = `${hours}:0${minutes}`;
+	} else {
+		time = `${hours}:${minutes}`;
+	}
 
-    return time;
+	return time;
 }
--- Day124_chat/view.js
+++ Day124_chat/view.js
@@ -1,11 +1,17 @@
 export const UI_ELEMENTS = {
-    messageTemplate: document.querySelector('.chat-content__message-template'),
-    sentMessageForm: document.querySelector('.chat-footer__form'),
-    chatContent: document.querySelector('.chat-content'),
-    optionsButton: document.querySelector('.chat-header__options-button'),
-    popups: {
-        popupOverlay: document.querySelector('.popup-wrapper'),
-        optionsPopup: document.querySelector('.options-popup'),
-        optionsCloseButton: document.querySelector('.options-popup__close-button'),
-    },
+	messageTemplate: document.querySelector('.chat-content__message-template'),
+	sentMessageForm: document.querySelector('.chat-footer__form'),
+	chatContent: document.querySelector('.chat-content'),
+	optionsButton: document.querySelector('.chat-header__options-button'),
+	authorizationButton: document.querySelector('.chat-header__authorization-button'),
+	popups: {
+		popupOverlay: document.querySelector('.popup-wrapper'),
+		optionsPopup: document.querySelector('.options-popup'),
+		optionsCloseButton: document.querySelector('.options-popup__close-button'),
+		authorizationPopup: document.querySelector('.authorization-popup'),
+		authorizationCloseButton: document.querySelector('.authorization-popup__close-button'),
+		authorizationForm: document.querySelector('.authorization-popup__mail-form'),
+		confirmationPopup: document.querySelector('.confirmation-popup'),
+		confirmationCloseButton: document.querySelector('.confirmation-popup__close-button'),
+	},
 };
