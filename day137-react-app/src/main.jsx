28f0e6cb168cc41e069171a4f424e8fcfcf63e63
Nick <dedsec1612@gmail.com>
2022-03-16 07:12:02 +0300

    react first look

---
+++ day137-react-app/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './App.css'
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // Эта привязка обязательна для работы `this` в колбэке.
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'Включено' : 'Выключено'}
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root')
+);
